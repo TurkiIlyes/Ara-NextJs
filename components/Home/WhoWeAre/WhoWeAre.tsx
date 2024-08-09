129d9718e67b4d653991b7aa78ed04764d3f294d
ilyess.torki.v <ilyess.torki.v@gmail.com>
2024-08-09 19:38:04 +0000

    update the project

--- components/Home/WhoWeAre/WhoWeAre.tsx
+++ components/Home/WhoWeAre/WhoWeAre.tsx
@@ -7,15 +7,16 @@ const WhoWeAre = () => {
         <span className=" text-2xl font-medium text-primary ">ARA COMPANY</span>
         <p className=" text-justify text-sm leading-loose font-medium text-grayscale-800 ">
           Fondée en 2019, ARA COMPANY est un leader dans le développement
-          d'applications pour l'industrie 4.0, la digitalisation, et la
-          robotique. Avec une équipe hautement qualifiée, elle transforme les
+          d&lsquo;applications pour l&lsquo;industrie 4.0, la digitalisation, et
+          la robotique. Avec une équipe hautement qualifiée, elle transforme les
           opérations industrielles en intégrant des technologies numériques
-          avancées comme l'IA et l'IoT. ARA COMPANY conçoit des solutions sur
-          mesure pour optimiser les processus, améliorer l'efficacité et
-          stimuler la croissance, en se concentrant sur la personnalisation pour
-          répondre aux besoins spécifiques de chaque client. Son engagement
-          envers l'innovation en fait un partenaire stratégique pour les
-          entreprises dans un monde de plus en plus connecté et automatisé.
+          avancées comme l&lsquo;IA et l&lsquo;IoT. ARA COMPANY conçoit des
+          solutions sur mesure pour optimiser les processus, améliorer
+          l&lsquo;efficacité et stimuler la croissance, en se concentrant sur la
+          personnalisation pour répondre aux besoins spécifiques de chaque
+          client. Son engagement envers l&lsquo;innovation en fait un partenaire
+          stratégique pour les entreprises dans un monde de plus en plus
+          connecté et automatisé.
         </p>
       </div>
       <div className=" w-7/12 relative hidden sm:flex flex-col gap-4 ">
